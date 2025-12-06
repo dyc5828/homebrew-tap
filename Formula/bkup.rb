0d4b27661388905a72acce1267b1b68e44e277b2
dyc5828 <dyc5828@gmail.com>
2025-12-06 14:46:28 -0800

    Update bkup to v1.1.0

--- Formula/bkup.rb
+++ Formula/bkup.rb
@@ -1,8 +1,8 @@
 class Bkup < Formula
   desc "Simple CLI tool for backing up files and directories"
   homepage "https://github.com/dyc5828/bkup-cli"
-  url "https://github.com/dyc5828/bkup-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "d4d9045602611d24aa64fbceb47658aad4dfa19c6a9ca852a36bcdd235ed3526"
+  url "https://github.com/dyc5828/bkup-cli/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "2a301144cbff4c251b0574b8be70939f57b203ce4e86aebcd434077e4e10d05d"
   license "MIT"
 
   def install
