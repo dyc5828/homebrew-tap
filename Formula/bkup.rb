9a9ef8afd05a7528a18833daf270098dba33a122
dyc5828 <dyc5828@gmail.com>
2025-12-19 19:42:51 -0800

    bkup 1.3.0: add zsh completions

--- Formula/bkup.rb
+++ Formula/bkup.rb
@@ -1,12 +1,13 @@
 class Bkup < Formula
   desc "Simple CLI tool for backing up files and directories"
   homepage "https://github.com/dyc5828/bkup-cli"
-  url "https://github.com/dyc5828/bkup-cli/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "495da98b351feb7fdbd0d24d4abfd786a0b2264a74728877ee990873d4fc0789"
+  url "https://github.com/dyc5828/bkup-cli/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "be4864941aabfe302a96dce6480ec7e399814de3b771c290dab0f1dc89377c7e"
   license "MIT"
 
   def install
     bin.install "bkup"
+    zsh_completion.install "completions/_bkup"
   end
 
   test do
